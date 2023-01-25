6d864342bd06a181717f90d1c9b4d7009e8fda23
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-01-25 15:52:19 +0000

    Add clickable buttons that show what the user picks and tested them in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-
 const buttons= document.querySelectorAll('.btn-circle');
 
-console.log(pickRandomChoice());
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+let score = 0;
+let userChoice = undefined;
+
+buttons.forEach(button => {
+  button.addEventListener('click',() => {
+     userChoice = button.getAttribute('data-choice');
+
+     console.log(userChoice);
+    }); 
+});
 
 function pickRandomChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
