cd6a837864fc6c4d1297290b4f52194200f23c08
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-01-25 15:42:03 +0000

    Add query selector command for the rock paper scissors buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+const buttons= document.querySelectorAll('.btn-circle');
+
+console.log(pickRandomChoice());
+
+function pickRandomChoice() {
+    return choices[Math.floor(Math.random() * choices.length)]
+    ;
+}
